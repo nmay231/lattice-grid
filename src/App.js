4c20d2b410bda9e254e2852c141ba4aa7dd42b89
Noah May <noahmouse2011@gmail.com>
2021-09-16 15:26:09 -0600

    Switched to CSS modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { PuzzleManager } from "./logic/PuzzleManager";
+import styles from "./App.module.css";
 
 export const App = () => {
     const canvas = useRef();
@@ -10,8 +11,8 @@ export const App = () => {
     }, []);
 
     return (
-        <div id="main-container">
-            <canvas ref={canvas}></canvas>
+        <div id="main-container" className={styles["main-container"]}>
+            <canvas className={styles.canvas} ref={canvas}></canvas>
         </div>
     );
 };
