2944ac7d11d77cc0ea64677ba33efcbb3eca30f0
Noah May <noahmouse2011@gmail.com>
2022-02-12 23:43:55 -0700

    Better Save/Cancel buttons for layer settings

--- src/components/SideBar/LayersGroup/CurrentLayerSettings.jsx
+++ src/components/SideBar/LayersGroup/CurrentLayerSettings.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useMemo, useState } from "react";
+import { isEqual } from "lodash";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { availableLayers } from "../../../logic/layers";
 import { JsonFormsWrapper } from "../../JsonFormsWrapper";
@@ -6,39 +7,40 @@ import { JsonFormsWrapper } from "../../JsonFormsWrapper";
 export const CurrentLayerSettings = ({ puzzle }) => {
     const layers = useSelector((state) => state.puzzle.layers);
     const selectedLayer = useSelector((state) => state.puzzle.selectedLayer);
-    const [changed, setChanged] = useState(false);
+    const [data, setData] = useState(null);
 
-    const { initialData, schema, uischema } = useMemo(() => {
-        setChanged(false);
+    const { layerType, id } = layers[selectedLayer];
+    const layer = puzzle.layers[id];
+    const layerClass = availableLayers[layerType];
 
-        const { layerType, id } = layers[selectedLayer];
-        const layer = puzzle.layers[id];
-        const layerClass = availableLayers[layerType];
-        console.log(layer);
+    const schema = layerClass.settingsSchema;
+    const uischema = {
+        type: "VerticalLayout",
+        elements: layerClass.settingsUISchemaElements,
+    };
+    const changed = !isEqual(data, layer.rawSettings);
 
-        return {
-            initialData: layer.rawSettings,
-            schema: layerClass.settingsSchema,
-            uischema: {
-                type: "VerticalLayout",
-                elements: layerClass.settingsUISchemaElements,
-            },
-        };
-    }, [layers, puzzle.layers, selectedLayer]);
-
-    const [data, setData] = useState(initialData);
     useEffect(() => {
-        setData(initialData);
-        setChanged(false);
-    }, [initialData]);
+        setData(layer.rawSettings);
+    }, [layer.rawSettings]);
+
+    if (!data) {
+        return <></>;
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
         puzzle.changeLayerSettings(layers[selectedLayer].id, data);
+        setData(null); // Trigger render
+
         // TODO: This can call .redrawScreen() twice if changing settings adds/removes objects as well as changing how the objects are displayed.
         // e.g. using the ToggleCharacters() layer, you might change the allowed characters and the displayStyle/positioning and that will call it twice: once from .changeLayerSettings (calling ControlsManager.handleLayerActions) and a second time directly in this function.
         // TODO: Changes = []
         puzzle.redrawScreen([]);
+    };
+
+    const handleCancel = () => {
+        setData(layer.rawSettings);
     };
 
     // TODO: Handle when no layers are present.
@@ -48,15 +50,21 @@ export const CurrentLayerSettings = ({ puzzle }) => {
                 <JsonFormsWrapper
                     data={data}
                     setData={(newData) => {
-                        console.log(`Change data`, newData);
-                        console.log();
                         setData(newData);
-                        setChanged(true);
                     }}
                     schema={schema}
                     uischema={uischema}
                 />
-                {changed && <button type="submit">Save</button>}
+                <button type="submit" disabled={!changed}>
+                    Save
+                </button>
+                <button
+                    type="button"
+                    disabled={!changed}
+                    onClick={handleCancel}
+                >
+                    Cancel
+                </button>
             </form>
         </div>
     );
