6e22224da048567d819a477f69baa70de71f8a1e
Noah May <noahmouse2011@gmail.com>
2022-05-22 10:48:53 -0500

    Convert multiPoint to expand and shrink in the same motion

--- src/logic/layers/controls/multiPoint.spec.ts
+++ src/logic/layers/controls/multiPoint.spec.ts
@@ -1,33 +1,297 @@
-import { handleEventsUnorderedSets } from "./multiPoint";
+import { getEventEssentials } from "../../../utils/testUtils";
+import { LayerStorage } from "../../StorageManager";
+import { ILayer, LayerEvent, PointerMoveOrDown } from "../baseLayer";
+import { DummyLayer } from "../_DummyLayer";
+import { handleEventsUnorderedSets, MinimalState } from "./multiPoint";
+
+type multiPointLayer<
+    ObjectState extends MinimalState = MinimalState,
+    RawSettings = object,
+> = ILayer<ObjectState, RawSettings>;
 
 describe("multiPoint.handleEventsUnorderedSets", () => {
-    if (false) console.log(handleEventsUnorderedSets);
+    const getFakeLayer = () => {
+        const layer = Object.create(DummyLayer) as multiPointLayer;
+        return layer;
+    };
 
-    // Remember to draw points in an unsorted order
-    // How to handle ids changing as an object is drawn... I guess I could still "snapshot" it, and just update tests when things change
-    it.todo("should draw a new object when none were selected");
+    type SecondArg = Parameters<typeof handleEventsUnorderedSets>[1];
+    const applySettings = (layer: multiPointLayer, arg?: SecondArg) =>
+        handleEventsUnorderedSets(layer, {
+            pointTypes: ["cells"],
+            ...arg,
+        });
 
-    it.todo("should draw a new object when a previous one was selected");
+    const getPointerEvent = (
+        event: Pick<PointerMoveOrDown, "type">,
+    ): PointerMoveOrDown => ({
+        ctrlKey: false,
+        shiftKey: false,
+        altKey: false,
+        cursor: { x: 1, y: 1 },
+        points: [],
+        ...event,
+    });
 
-    it.todo(
-        "should expand, shrink, and expand an object that was previously selected",
-    );
+    // TODO: This and the other controls tests might have to be rewritten to be more clear and more consistent. It's pretty much a hodge-pogge of assertions at the moment, which I guess is better than nothing for now...
+    // For example, we have to call layer.gatherPoints each time because it's not a pure function. It's purposely not pure, but that doesn't have to be if modified appropriately.
 
-    it.todo(
-        "should expand, shrink, and expand an object that was not previously selected",
-    );
+    it("should draw a new single-point object when none were selected", () => {
+        const layer = getFakeLayer();
+        applySettings(layer);
 
-    it.todo(
-        "should shrink, expand, and shrink an object that was previously selected",
-    );
+        const selectPoints = jest.fn();
+        const getBatchId = jest.fn();
+        const essentials = getEventEssentials<MinimalState>();
+        essentials.grid.selectPointsWithCursor = selectPoints;
+        essentials.storage.getNewBatchId = getBatchId;
 
-    it.todo(
-        "should shrink, expand, and shrink an object that was not previously selected",
-    );
+        selectPoints.mockReturnValueOnce(["a"]);
+        let fakeEvent: LayerEvent<MinimalState> = {
+            ...essentials,
+            ...getPointerEvent({ type: "pointerDown" }),
+        };
 
-    it.todo("should remove a point from an object after a simple click");
+        let points = layer.gatherPoints(fakeEvent);
+        expect(points).toEqual(["a"]);
+
+        getBatchId.mockReturnValueOnce(1);
+        let result = layer.handleEvent({ ...fakeEvent, points });
+        expect(result.history).toEqual([
+            { batchId: 1, id: "a", object: { points: ["a"], state: null } },
+        ]);
+        expect(result.discontinueInput).toBeFalsy();
+
+        essentials.stored.objects.a = result.history?.[0].object;
+        essentials.stored.renderOrder.push("a");
+
+        result = layer.handleEvent({ ...fakeEvent, type: "pointerUp" });
+        expect(result.history?.length).toBeFalsy();
+        expect(result.discontinueInput).toBeTruthy();
+    });
+
+    it("should draw a new object when a previous one was selected", () => {
+        const layer = getFakeLayer();
+        applySettings(layer);
+
+        const stored: LayerStorage<MinimalState> = {
+            objects: { a: { id: "a", points: ["a"], state: null } },
+            renderOrder: ["a"],
+        };
+        const selectPoints = jest.fn();
+        const getBatchId = jest.fn();
+        const essentials = getEventEssentials<MinimalState>({ stored });
+        essentials.grid.selectPointsWithCursor = selectPoints;
+        essentials.storage.getNewBatchId = getBatchId;
+
+        // Select the existing object
+        essentials.stored.currentObjectId = "a";
+
+        selectPoints.mockReturnValueOnce(["b"]);
+        let fakeEvent: LayerEvent<MinimalState> = {
+            ...essentials,
+            ...getPointerEvent({ type: "pointerDown" }),
+        };
+        let points = layer.gatherPoints(fakeEvent);
+        expect(points).toEqual(["b"]);
+
+        getBatchId.mockReturnValueOnce(1);
+        let result = layer.handleEvent({ ...fakeEvent, points });
+        expect(result.history).toEqual([
+            { batchId: 1, id: "b", object: { points: ["b"], state: null } },
+        ]);
+        expect(result.discontinueInput).toBeFalsy();
+        essentials.stored.objects.b = result.history?.[0].object;
+        essentials.stored.renderOrder.push("b");
+
+        result = layer.handleEvent({ ...fakeEvent, type: "pointerUp" });
+        expect(result.history?.length).toBeFalsy();
+        expect(result.discontinueInput).toBeTruthy();
+    });
+
+    it("should expand and shrink an object", () => {
+        const layer = getFakeLayer();
+        applySettings(layer);
+
+        const selectPoints = jest.fn();
+        const getBatchId = jest.fn();
+        const essentials = getEventEssentials<MinimalState>();
+        essentials.grid.selectPointsWithCursor = selectPoints;
+        essentials.storage.getNewBatchId = getBatchId;
+
+        // Start drawing the object
+        selectPoints.mockReturnValueOnce(["b"]);
+        let fakeEvent: LayerEvent<MinimalState> = {
+            ...essentials,
+            ...getPointerEvent({ type: "pointerDown" }),
+        };
+
+        let points = layer.gatherPoints(fakeEvent);
+        expect(points).toEqual(["b"]);
+
+        getBatchId.mockReturnValueOnce(1);
+        let result = layer.handleEvent({ ...fakeEvent, points });
+        expect(result.history).toEqual([
+            { batchId: 1, id: "b", object: { points: ["b"], state: null } },
+        ]);
+        expect(result.discontinueInput).toBeFalsy();
+
+        essentials.stored.objects.b = result.history?.[0].object;
+        essentials.stored.renderOrder.push("b");
+
+        // Expand the object
+        selectPoints.mockReturnValueOnce(["c", "a"]);
+        fakeEvent.type = "pointerMove";
+        points = layer.gatherPoints(fakeEvent);
+        expect(points).toEqual(["b", "c", "a"]);
+
+        result = layer.handleEvent({ ...fakeEvent, points });
+        points = [...points].sort();
+        expect(result.history).toEqual([
+            { batchId: 1, id: "b", object: { points, state: null } },
+        ]);
+        expect(result.discontinueInput).toBeFalsy();
+
+        essentials.stored.objects.b = result.history?.[0].object;
+
+        // Shrink the object
+        selectPoints.mockReturnValueOnce(["b"]);
+        fakeEvent.type = "pointerMove";
+        points = layer.gatherPoints(fakeEvent);
+        expect(points).toEqual(["a", "b"]);
+
+        result = layer.handleEvent({ ...fakeEvent, points });
+        points = ["b", "c"];
+        expect(result.history).toEqual([
+            { batchId: 1, id: "b", object: { points, state: null } },
+        ]);
+        expect(result.discontinueInput).toBeFalsy();
+
+        essentials.stored.objects.b = result.history?.[0].object;
+
+        result = layer.handleEvent({ ...fakeEvent, type: "pointerUp" });
+        expect(result.history).toEqual([
+            { batchId: 1, id: "b", object: null },
+            {
+                batchId: 1,
+                id: "b;c",
+                object: { id: "b;c", points, state: null },
+            },
+        ]);
+        expect(result.discontinueInput).toBeTruthy();
+    });
+
+    it("should remove a point from an object after a simple click", () => {
+        const layer = getFakeLayer();
+        applySettings(layer);
+
+        const stored: LayerStorage<MinimalState> = {
+            objects: { "a;b": { id: "a;b", points: ["a", "b"], state: null } },
+            renderOrder: ["a;b"],
+            currentObjectId: "a;b",
+        };
+        const selectPoints = jest.fn();
+        const getBatchId = jest.fn();
+        const essentials = getEventEssentials<MinimalState>({ stored });
+        essentials.grid.selectPointsWithCursor = selectPoints;
+        essentials.storage.getNewBatchId = getBatchId;
+
+        selectPoints.mockReturnValueOnce(["b"]);
+        let fakeEvent: LayerEvent<MinimalState> = {
+            ...essentials,
+            ...getPointerEvent({ type: "pointerDown" }),
+        };
+        let points = layer.gatherPoints(fakeEvent);
+        expect(points).toEqual(["b"]);
+
+        getBatchId.mockReturnValueOnce(1);
+        let result = layer.handleEvent({ ...fakeEvent, points });
+        expect(result.history).toEqual([
+            // TODO: Required to force a rerender
+            {
+                batchId: 1,
+                id: "a;b",
+                object: { id: "a;b", points: ["a", "b"], state: null },
+            },
+        ]);
+        expect(result.discontinueInput).toBeFalsy();
+
+        result = layer.handleEvent({ ...fakeEvent, type: "pointerUp" });
+        expect(result.history).toEqual([
+            { batchId: 1, id: "a;b", object: null },
+            {
+                batchId: 1,
+                id: "a",
+                object: { id: "a", points: ["a"], state: null },
+            },
+        ]);
+        expect(result.discontinueInput).toBeTruthy();
+    });
+
+    it("should not remove the starting point from an object if points were added/deleted", () => {
+        const layer = getFakeLayer();
+        applySettings(layer);
+
+        const stored: LayerStorage<MinimalState> = {
+            objects: { "a;b": { id: "a;b", points: ["a", "b"], state: null } },
+            renderOrder: ["a;b"],
+            currentObjectId: "a;b",
+        };
+        const selectPoints = jest.fn();
+        const getBatchId = jest.fn();
+        const essentials = getEventEssentials<MinimalState>({ stored });
+        essentials.grid.selectPointsWithCursor = selectPoints;
+        essentials.storage.getNewBatchId = getBatchId;
+
+        // Select the starting point
+        selectPoints.mockReturnValueOnce(["b"]);
+        let fakeEvent: LayerEvent<MinimalState> = {
+            ...essentials,
+            ...getPointerEvent({ type: "pointerDown" }),
+        };
+        let points = layer.gatherPoints(fakeEvent);
+
+        getBatchId.mockReturnValueOnce(1);
+        let result = layer.handleEvent({ ...fakeEvent, points });
+
+        // Expand
+        selectPoints.mockReturnValueOnce(["c", "d"]);
+        fakeEvent.type = "pointerMove";
+        points = layer.gatherPoints(fakeEvent);
+
+        result = layer.handleEvent({ ...fakeEvent, points });
+        essentials.stored.objects["a;b"] = result.history?.[0].object;
+
+        // Shrink
+        selectPoints.mockReturnValueOnce(["d", "b"]);
+        points = layer.gatherPoints(fakeEvent);
+
+        result = layer.handleEvent({ ...fakeEvent, points });
+        essentials.stored.objects["a;b"] = result.history?.[0].object;
+
+        // End on the starting point
+        result = layer.handleEvent({ ...fakeEvent, type: "pointerUp" });
+        expect(result.history).toEqual([
+            { batchId: 1, id: "a;b", object: null },
+            {
+                batchId: 1,
+                // Point b should remain even though the event started and ended on it
+                id: "a;b;c",
+                object: { id: "a;b;c", points: ["a", "b", "c"], state: null },
+            },
+        ]);
+        expect(result.discontinueInput).toBeTruthy();
+    });
 
     it.todo("should delete a single-point object after a simple click");
 
+    it.todo(
+        "should delete the layers state then delete the object using the delete key",
+    );
+
     it.todo("should deselect an object when escape is pressed");
+
+    it.todo("should select an object on undo/redo when object exists");
+
+    it.todo("should select an object on undo/redo when object does not exist");
 });
--- src/logic/layers/controls/multiPoint.ts
+++ src/logic/layers/controls/multiPoint.ts
@@ -1,6 +1,4 @@
-// 1.5?) Figure out how to show current killercages (I added getOverlayBlits)? Then again, the current system should still work, but the blits won't be guaranteed to be above everything else (which is actually not a desired behavior. maybe I never want to change to that...)
-// 2) Convert to typescript and basically rewrite from scratch.
-
+import { cloneDeep } from "lodash";
 import { ILayer } from "../baseLayer";
 import { KeyDownEventHandler } from "../Selection";
 
@@ -39,8 +37,7 @@ export const handleEventsUnorderedSets = <
 
     layer.gatherPoints = (event) => {
         const { grid, tempStorage } = event;
-        tempStorage.blacklist = tempStorage.blacklist ?? [];
-        let newPoints = grid.selectPointsWithCursor({
+        const newPoints = grid.selectPointsWithCursor({
             cursor: event.cursor,
             pointTypes,
             deltas,
@@ -48,18 +45,11 @@ export const handleEventsUnorderedSets = <
         });
         if (!newPoints.length) return [];
 
-        const previousPoint = tempStorage.previousPoint;
+        if (tempStorage.previousPoint) {
+            newPoints.unshift(tempStorage.previousPoint);
+        }
         tempStorage.previousPoint = newPoints[newPoints.length - 1];
 
-        newPoints = newPoints.filter(
-            (id) => tempStorage.blacklist.indexOf(id) === -1,
-        );
-        if (!newPoints.length) return [];
-        tempStorage.blacklist.push(...newPoints);
-
-        if (previousPoint) {
-            newPoints.unshift(previousPoint);
-        }
         return newPoints;
     };
 
@@ -71,172 +61,142 @@ export const handleEventsUnorderedSets = <
         const stored = storage.getStored<ObjectState>({ layer, grid });
         const currentObjectId = stored.currentObjectId;
         if (currentObjectId === undefined && type !== "pointerDown") {
-            return {};
+            return {}; // Other events only matter if there is an object selected
         }
         const object = stored.objects[currentObjectId];
 
         switch (type) {
-            case "delete": {
-                stored.currentObjectId = undefined;
-
-                // TODO: allow the layer to "delete" it's state before deleting the object?
-                const history = [{ id: currentObjectId, object: null }];
-                return { discontinueInput: true, history };
-            }
-            case "cancelAction": {
-                stored.currentObjectId = undefined;
-
-                // TODO: How do I force rerender without polluting history
-                const history = [{ id: currentObjectId, object }];
-                return { discontinueInput: true, history };
-            }
             case "keyDown": {
                 return handleKeyDown?.({ ...event, points: [] }) || {};
             }
-            case "pointerDown":
-            case "pointerMove": {
+            case "delete": {
+                const result = handleKeyDown?.({ ...event, points: [] });
+                if (result?.history?.length) {
+                    // Allow the layer to delete its state before deleting the object itself.
+                    return result;
+                }
+                stored.currentObjectId = undefined;
+                return {
+                    discontinueInput: true,
+                    history: [{ id: currentObjectId, object: null }],
+                };
+            }
+            case "cancelAction": {
+                stored.currentObjectId = undefined;
+                return {
+                    discontinueInput: true,
+                    history: [
+                        // Force a rerender without polluting history
+                        { id: currentObjectId, batchId: "ignore", object },
+                    ],
+                };
+            }
+            case "pointerDown": {
+                tempStorage.batchId = storage.getNewBatchId();
+                const batchId = tempStorage.batchId;
+                // There's only one point with pointerDown
                 const startPoint = event.points[0];
                 const overlap = stored.renderOrder.filter(
                     (id) => stored.objects[id].points.indexOf(startPoint) > -1,
                 );
 
-                if (currentObjectId === undefined) {
-                    // This is only called on pointerDown, so there is only one new point
-                    const point = startPoint;
+                if (overlap.length) {
+                    // Select the topmost existing object
+                    const id = overlap[overlap.length - 1];
+                    tempStorage.removeSingle = true;
+                    stored.currentObjectId = id;
 
-                    if (overlap.length) {
-                        // Select the topmost existing object
-                        const id = overlap[overlap.length - 1];
-                        stored.currentObjectId = id;
-                        return {
-                            // Prevent interaction until user clicks/taps again
-                            discontinueInput: true,
-                            // Move object to the front of the stack
-                            history: [{ id, object: stored.objects[id] }],
-                        };
-                    } else {
-                        // Start drawing new object
-                        stored.currentObjectId = point;
-                        tempStorage.expanding = true;
-                        return {
-                            history: [
-                                // TODO: state=null vs state=layer.getState(null)
-                                {
-                                    id: point,
-                                    object: { points: [point], state: null },
-                                },
-                            ],
-                        };
-                    }
-                }
-
-                const points = object.points;
-                if (
-                    type === "pointerDown" &&
-                    points.indexOf(startPoint) === -1
-                ) {
-                    let discontinueInput = false;
-                    const history = [];
-                    const newId = object.points.join(";");
-                    if (currentObjectId !== newId) {
-                        history.push(
-                            { id: currentObjectId, object: null },
-                            { id: newId, object },
-                        );
-                    } else {
-                        // Force rerender of current object
-                        // TODO: How do I force rerender without polluting history (currentObjectId changed, but the object state has not). Then again, it does need to affect history because reordering objects in renderOrder is changing the puzzle (if allowOverlap=true).
-                        history.push({ id: object.id, object });
-                    }
-
-                    if (overlap.length) {
-                        // Select a different existing object
-                        stored.currentObjectId = overlap[overlap.length - 1];
-                        discontinueInput = true;
-                    } else {
-                        // Create a new one
-                        const id = startPoint;
-                        stored.currentObjectId = id;
-                        tempStorage.expanding = true;
-                        history.push({
-                            id,
-                            object: { points: [id], state: null },
-                        });
-                    }
-                    return { history, discontinueInput };
-                } else if (type === "pointerDown") {
-                    // We start to resize the current object, but we don't know yet if it's expanding or shrinking
-                    return {};
-                }
-                // Here, we're definitely resizing the object
-
-                if (tempStorage.expanding === undefined) {
-                    // Only expand the object if the newPoints are not part of the old object
-                    // Note that startPoint is actually the last point from the previous event
-                    tempStorage.expanding =
-                        points.indexOf(event.points[1]) === -1;
-                }
-
-                const updatedPoints = tempStorage.expanding
-                    ? points
-                          .concat(
-                              event.points.filter(
-                                  (point) => points.indexOf(point) === -1,
-                              ),
-                          )
-                          .sort()
-                    : points.filter(
-                          (point) => event.points.indexOf(point) === -1,
-                      );
-
-                if (!updatedPoints.length) {
-                    stored.currentObjectId = undefined;
+                    // Force a rerender without polluting history
                     return {
-                        discontinueInput: true,
-                        history: [{ id: currentObjectId, object: null }],
+                        history: [{ id, object: stored.objects[id], batchId }],
                     };
                 }
-                const newObject = {
-                    points: updatedPoints,
-                    state: object.state,
+
+                // Start drawing a new object
+                tempStorage.removeSingle = false;
+                stored.currentObjectId = startPoint;
+                return {
+                    history: [
+                        {
+                            id: startPoint,
+                            batchId,
+                            object: { points: [startPoint], state: null },
+                        },
+                    ],
                 };
+            }
+            case "pointerMove": {
+                tempStorage.removeSingle = false;
+
+                const newPoints = new Set(object.points);
+                let previous = event.points[0];
+                for (let next of event.points.slice(1)) {
+                    if (newPoints.has(next)) {
+                        // Shrink by removing the previous point
+                        newPoints.delete(previous);
+                    } else {
+                        // Expand by including it
+                        newPoints.add(previous);
+                    }
+                    previous = next;
+                }
+
+                // Ensure the last point is always part of the object
+                if (!newPoints.has(previous)) {
+                    newPoints.add(previous);
+                }
 
                 return {
-                    history: [{ id: currentObjectId, object: newObject }],
+                    history: [
+                        {
+                            id: currentObjectId,
+                            batchId: tempStorage.batchId,
+                            object: {
+                                ...object,
+                                points: [...newPoints].sort(),
+                            },
+                        },
+                    ],
                 };
             }
             case "pointerUp": {
                 if (currentObjectId === undefined) {
                     return { discontinueInput: true };
                 }
+                const batchId = tempStorage.batchId;
 
-                const objectCopy = { ...object };
-                if (tempStorage.expanding === undefined) {
+                const objectCopy = cloneDeep(object);
+                if (tempStorage.removeSingle) {
                     // Remove the one cell that was selected
                     objectCopy.points = objectCopy.points.filter(
                         (p) => p !== tempStorage.previousPoint,
                     );
 
+                    // Delete the object if empty
                     if (!objectCopy.points.length) {
                         return {
                             discontinueInput: true,
-                            history: [{ id: currentObjectId, object: null }],
+                            history: [
+                                { id: currentObjectId, batchId, object: null },
+                            ],
                         };
                     }
                 }
 
                 const oldId = currentObjectId;
+                objectCopy.points.sort();
                 const newId = objectCopy.points.join(";");
                 if (oldId === newId) {
                     return { discontinueInput: true };
                 }
 
                 stored.currentObjectId = newId;
+                objectCopy.id = newId;
                 return {
                     discontinueInput: true,
                     history: [
-                        { id: oldId, object: null },
-                        { id: newId, object: objectCopy },
+                        { id: oldId, batchId, object: null },
+                        { id: newId, batchId, object: objectCopy },
                     ],
                 };
             }
