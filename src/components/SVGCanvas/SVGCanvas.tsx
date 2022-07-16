97f27ea296d5a14ec3eb43c8dec9874fb5ef76d8
Noah May <noahmouse2011@gmail.com>
2022-07-16 01:13:06 -0500

    Do not fit canvas to the full screen

--- src/components/SVGCanvas/SVGCanvas.tsx
+++ src/components/SVGCanvas/SVGCanvas.tsx
@@ -20,34 +20,33 @@ export const SVGCanvas = () => {
     const layers = useAtomValue(layersAtom).layers;
     const { minX, minY, width, height } = useAtomValue(canvasSizeAtom);
 
-    // TODO: As neat as staying in a square is, I'll probably need to switch to using a scrollbar since users can have multiple grids and I shouldn't break users expectations...
     return (
-        <div
-            className={styling.svgContainer}
-            style={{
-                width: `min(80vw, ${width}/${height}*100vh)`,
-                height: `min(100vh, ${height}/${width}*80vw)`,
-            }}
-        >
-            <svg
-                {...controls.eventListeners}
-                className={styling.svg}
-                viewBox={`${minX} ${minY} ${width} ${height}`}
+        <div className={styling.scrollArea}>
+            <div
+                className={styling.outerContainer}
+                style={{ width: `${width}px` }}
             >
-                {layers.flatMap(({ id }) =>
-                    blitGroups[id].map((group) => {
-                        const Blitter = blitters[group.blitter];
-                        return (
-                            <Blitter
-                                blits={group.blits}
-                                // I was hoping typescript would be smarter...
-                                style={group.style as any}
-                                key={id + group.id}
-                            />
-                        );
-                    }),
-                )}
-            </svg>
+                <div
+                    className={styling.innerContainer}
+                    {...controls.eventListeners}
+                >
+                    <svg viewBox={`${minX} ${minY} ${width} ${height}`}>
+                        {layers.flatMap(({ id }) =>
+                            blitGroups[id].map((group) => {
+                                const Blitter = blitters[group.blitter];
+                                return (
+                                    <Blitter
+                                        blits={group.blits}
+                                        // I was hoping typescript would be smarter...
+                                        style={group.style as any}
+                                        key={id + group.id}
+                                    />
+                                );
+                            }),
+                        )}
+                    </svg>
+                </div>
+            </div>
         </div>
     );
 };
