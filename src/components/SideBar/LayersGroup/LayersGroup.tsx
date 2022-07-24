a50d537d52785d17b38d6d6632bf88865fcec31f
Noah May <noahmouse2011@gmail.com>
2022-07-24 04:34:22 -0500

    Move reset-puzzle button

--- src/components/SideBar/LayersGroup/LayersGroup.tsx
+++ src/components/SideBar/LayersGroup/LayersGroup.tsx
@@ -1,26 +1,11 @@
-import { usePuzzle } from "../../../atoms/puzzle";
-import { blurActiveElement } from "../../../utils/DOMUtils";
 import { Group } from "../Group";
 import { AddNewLayerButton } from "./AddNewLayerButton";
 import { LayerConstraintSettings } from "./LayerConstraintSettings";
 import { LayerList } from "./LayerList";
 
 export const LayersGroup = () => {
-    const puzzle = usePuzzle();
-
     return (
         <Group name="Layers" expanded>
-            <button
-                onClick={() => {
-                    // Temporary in it's current form
-                    puzzle.freshPuzzle();
-                    puzzle.resizeCanvas();
-                    puzzle.renderChange({ type: "draw", layerIds: "all" });
-                    blurActiveElement();
-                }}
-            >
-                Reset Puzzle
-            </button>
             <AddNewLayerButton />
             <LayerList />
             <hr />
--- src/components/SideBar/MainGroup/MainGroup.tsx
+++ src/components/SideBar/MainGroup/MainGroup.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@mantine/core";
+import { usePuzzle } from "../../../atoms/puzzle";
+import { blurActiveElement } from "../../../utils/DOMUtils";
 import { Group } from "../Group";
 import { ResizeGridButton } from "./ResizeGridButton";
 
 export const MainGroup = () => {
+    const puzzle = usePuzzle();
+
     return (
         <Group name="Puzzle" expanded>
             <div
@@ -13,6 +18,19 @@ export const MainGroup = () => {
                 }}
             >
                 <ResizeGridButton />
+
+                <Button
+                    mt="sm"
+                    onClick={() => {
+                        // Temporary in it's current form
+                        puzzle.freshPuzzle();
+                        puzzle.resizeCanvas();
+                        puzzle.renderChange({ type: "draw", layerIds: "all" });
+                        blurActiveElement();
+                    }}
+                >
+                    Reset Puzzle
+                </Button>
             </div>
         </Group>
     );
