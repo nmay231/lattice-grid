d23869daad5f89131b07deb8e2101902fa673507
Noah May <noahmouse2011@gmail.com>
2022-11-21 13:09:26 -0600

    Vite works without JSONForms

--- src/components/JsonFormsWrapper/JsonFormsWrapper.tsx
+++ src/components/JsonFormsWrapper/JsonFormsWrapper.tsx
@@ -1,8 +1,12 @@
-import { JsonSchema, UISchemaElement } from "@jsonforms/core";
-import { JsonForms } from "@jsonforms/react";
-import { vanillaCells, vanillaRenderers } from "@jsonforms/vanilla-renderers";
+// import { JsonSchema, UISchemaElement } from "@jsonforms/core";
+// import { JsonForms } from "@jsonforms/react";
+// import { vanillaCells, vanillaRenderers } from "@jsonforms/vanilla-renderers";
 import { useRef } from "react";
-import { UnknownObject } from "../../types";
+import { NeedsUpdating, UnknownObject } from "../../types";
+
+// Try disabling anything to do with JSONForms, then I know I just need a replacement
+type JsonSchema = NeedsUpdating;
+type UISchemaElement = NeedsUpdating;
 
 type Props = {
     data: UnknownObject;
@@ -19,18 +23,19 @@ export const JsonFormsWrapper: React.FC<Props> = ({ data, setData, schema, uisch
     const firstRender = useRef(true);
 
     return (
-        <JsonForms
-            schema={schema}
-            uischema={uischema}
-            data={data}
-            cells={vanillaCells}
-            renderers={vanillaRenderers}
-            onChange={({ data, errors }) => {
-                if (!errors?.length && !firstRender.current) {
-                    setData(data);
-                }
-                firstRender.current = false;
-            }}
-        />
+        <div>JSONForms, baby1</div>
+        // <JsonForms
+        //     schema={schema}
+        //     uischema={uischema}
+        //     data={data}
+        //     cells={vanillaCells}
+        //     renderers={vanillaRenderers}
+        //     onChange={({ data, errors }) => {
+        //         if (!errors?.length && !firstRender.current) {
+        //             setData(data);
+        //         }
+        //         firstRender.current = false;
+        //     }}
+        // />
     );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgrPlugin from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgrPlugin()],
-    server: { port: 3000 },
-    // build: {outDir: "dist"}
+    server: { port: 3000, strictPort: true },
 });
