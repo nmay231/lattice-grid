f93db445c737cd7887fdd1403d29a98517650335
Noah May <noahmouse2011@gmail.com>
2023-01-13 08:18:23 -0600

    Remove the debug focus group until pageMode is implemented.

--- src/components/SideBar/ConstraintsGroup.tsx
+++ src/components/SideBar/ConstraintsGroup.tsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import { usePuzzle } from "../../state/puzzle";
-import { useFocusElementHandler, useFocusGroup } from "../../utils/focusManagement";
 import { BlocklyModalButton } from "../Blockly/BlocklyModal";
 import { Group } from "./Group";
 
 export const CodeGroup = () => {
-    const puzzle = usePuzzle();
-    const { ref } = useFocusGroup({ puzzle, group: "debug" });
-    const { ref: buttonRef, unfocus } = useFocusElementHandler();
-    const [state, setState] = useState([..."12345"]);
-
     return (
         <Group name="Constraints" expanded>
             <div
@@ -21,38 +13,6 @@ export const CodeGroup = () => {
                 }}
             >
                 <BlocklyModalButton>Show Constraints</BlocklyModalButton>
-                {/*  */}
-                {/* Temporary list of inputs to test global focus management */}
-                <button tabIndex={-1} onClick={() => console.log(-1)}>
-                    tabIndex=-1
-                </button>
-                <button
-                    ref={buttonRef}
-                    tabIndex={0}
-                    onClick={() => {
-                        unfocus();
-                        console.log(0);
-                    }}
-                >
-                    tabIndex=0
-                </button>
-                Press delete to test removing the currently focused element.
-                <div id="other" ref={ref}>
-                    {/* TODO: Hack Mantine's useFocusTrap so it doesn't focus the first element right away */}
-                    <div data-autofocus></div>
-                    {state.map((x) => (
-                        <input
-                            key={x}
-                            placeholder={x}
-                            onKeyDown={(e) => {
-                                if (e.key === "Delete") {
-                                    console.log("deleted");
-                                    setState(state.filter((y) => y !== x));
-                                }
-                            }}
-                        />
-                    ))}
-                </div>
             </div>
         </Group>
     );
