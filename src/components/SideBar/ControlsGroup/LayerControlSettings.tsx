2003ef30db7fd5edd3ebc98d1d1f7240e90078d7
Noah May <noahmouse2011@gmail.com>
2023-01-14 17:43:59 -0600

    Clean up constraint and control settings

--- src/components/SideBar/ControlsGroup/LayerControlSettings.tsx
+++ src/components/SideBar/ControlsGroup/LayerControlSettings.tsx
@@ -1,46 +1,17 @@
-import { useEffect, useState } from "react";
 import { useSnapshot } from "valtio";
 import { availableLayers } from "../../../layers";
-import { constraintSettingsProxy } from "../../../state/constraintSettings";
-
 import { usePuzzle } from "../../../state/puzzle";
-import { UnknownObject } from "../../../types";
+import { JSONSchema, Layer, LayerClass, UnknownObject } from "../../../types";
 import { useFocusGroup } from "../../../utils/focusManagement";
-import { valtioRef } from "../../../utils/imports";
 import { JsonFormsWrapper } from "../../JsonFormsWrapper";
 
-export const LayerControlSettings = () => {
+type InnerProps = { layer: Layer; controls: JSONSchema };
+
+const _LayerControlSettings = ({ layer, controls }: InnerProps) => {
     const puzzle = usePuzzle();
-
-    const snap = useSnapshot(puzzle.layers);
-    const id = snap.currentKey;
-    const layer = id && puzzle.layers.get(id);
-
-    const [data, setData] = useState<UnknownObject | null>(null);
-    const settingsSnap = useSnapshot(constraintSettingsProxy);
-
-    // We want to update a layer's settings whenever data changes, but changing id also changes data.
-    // So we keep track of the data and only update settings when data changes but id doesn't.
-    const [lastId, setLastId] = useState(id);
-
-    useEffect(() => {
-        if (layer) {
-            setData(layer.rawSettings);
-        }
-        // Putting settingsSnap.settings as a render dependency is necessary to update this component's `data`
-        // TODO: This doesn't seem necessary
-    }, [layer, settingsSnap.settings]);
-
     const { ref, unfocus } = useFocusGroup({ puzzle, group: "controlSettings" });
 
-    if (!data || !layer || !id) {
-        return <></>;
-    }
-
-    const layerType = snap.map[id].type;
-    const layerClass = availableLayers[layerType as keyof typeof availableLayers];
-
-    const { schema, uischemaElements } = layerClass.controls || {};
+    const { schema, uischemaElements } = controls;
     const uischema = { type: "VerticalLayout", elements: uischemaElements };
 
     return (
@@ -48,26 +19,35 @@ export const LayerControlSettings = () => {
             {/* TODO: Hack Mantine's useFocusTrap so it doesn't focus the first element right away */}
             <div data-autofocus></div>
             <JsonFormsWrapper
-                data={data}
-                setData={(newData: UnknownObject) => {
-                    setData(newData);
-                    // Constraint settings cannot be left out of date
-                    // TODO: PuzzleManager maybe should have a .selectLayer method. If so, this needs to go in that instead of this.
-                    // eslint-disable-next-line valtio/state-snapshot-rule
-                    constraintSettingsProxy.settings = valtioRef(newData);
-
-                    if (id !== lastId) {
-                        setLastId(id);
-                    } else {
-                        puzzle.changeLayerSettings(id, newData);
-                        puzzle.renderChange({ type: "draw", layerIds: [id] });
-                        unfocus();
-                    }
+                data={layer.rawSettings}
+                onChange={(newData: UnknownObject) => {
+                    puzzle.changeLayerSettings(layer.id, newData);
+                    puzzle.renderChange({ type: "draw", layerIds: [layer.id] });
+                    unfocus();
                 }}
                 schema={schema}
                 uischema={uischema}
             />
-            {layerClass.controls ? null : <i>No control settings for this layer</i>}
         </div>
     );
+};
+
+export const LayerControlSettings = () => {
+    const { layers } = usePuzzle();
+    const layersSnap = useSnapshot(layers);
+    const id = layersSnap.currentKey;
+    const layer = id && layers.get(id);
+
+    if (!layer) {
+        return <i>Add a layer to get started</i>;
+    }
+
+    const layerType = layer.type;
+    const layerClass = availableLayers[layerType as keyof typeof availableLayers] as LayerClass;
+
+    if (!layerClass.controls) {
+        return <i>This layer has no control settings</i>;
+    }
+
+    return <_LayerControlSettings key={id} layer={layer} controls={layerClass.controls} />;
 };
