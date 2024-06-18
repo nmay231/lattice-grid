ef2aec363c9fa085035f670ef95f5ad8b4e18b9a
Noah May <noahmouse2011@gmail.com>
2024-06-17 21:48:35 -0500

    Don't include / for shorter URLs

--- src/components/ImportExportModal/ImportExportModal.tsx
+++ src/components/ImportExportModal/ImportExportModal.tsx
@@ -74,7 +74,7 @@ export const ImportExportModal = React.memo(function ImportExportModal({
         if (opened) {
             const string = exportPuzzleData(puzzle, answerCheck);
             // TODO: Make `/edit` urls actually work.
-            return `${window.location.origin}/${exportPlay ? "" : "edit"}?0=${string}`;
+            return `${window.location.origin}${exportPlay ? "" : "/edit"}?0=${string}`;
         }
     }, [opened, puzzle, answerCheck, exportPlay]);
 
