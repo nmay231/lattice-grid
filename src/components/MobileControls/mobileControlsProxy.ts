cc03dcb3fec86ce590fd2abf124e1dfb033ee52f
Noah May <noahmouse2011@gmail.com>
2024-07-20 19:51:41 -0500

    Fix the sidebar closing when trying to change author or title of puzzle

--- src/components/MobileControls/mobileControlsProxy.ts
+++ src/components/MobileControls/mobileControlsProxy.ts
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { proxy } from "valtio";
-import { subscribeKey } from "valtio/utils";
+import { subscribeKey, useProxy } from "valtio/utils";
+import { focusProxy } from "../../utils/focusManagement";
 import { sidebarProxy, smallPageWidth } from "../SideBar/sidebarProxy";
 
 // TODO: Should this be a part of Mantine's theme and PuzzleManager respectively?
@@ -17,12 +18,16 @@ subscribeKey(mobileControlsProxy, "enabled", () => {
 
 // Only put in a useEffect to work with HMR
 export const useResizeObserver = () => {
+    const focus = useProxy(focusProxy);
     useEffect(() => {
         const func = () => {
             mobileControlsProxy.isSmallScreen = window.innerWidth < smallPageWidth;
             // TODO: Respect permanent setting, if I ever make one
             mobileControlsProxy.opened = mobileControlsProxy.isSmallScreen;
-            sidebarProxy.opened = !mobileControlsProxy.isSmallScreen;
+            // TODO: Renaming author or title on Android shrinks the screen, so we only close the sidebar if the layer list is focused. We only care to open and close the sidebar at all if we are resizing the window on desktop, so perhaps I should find a better way to manage those preferences than screen size alone.
+            if (focus.group === "layerList") {
+                sidebarProxy.opened = !mobileControlsProxy.isSmallScreen;
+            }
         };
         window.addEventListener("resize", func);
         return () => window.removeEventListener("resize", func);
